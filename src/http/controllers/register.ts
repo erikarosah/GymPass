04d45f36b0668e260370a85db5d90f7472f09aac
erikarosah <erikarosamg@gmail.com>
2023-12-26 17:50:39 -0300

    Tratando erros da aplicação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { RegisterUseCase } from '@/use-cases/register';
 import { PrismaUserRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
     const registerBodySchema = z.object({
@@ -21,8 +22,13 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
             email,
             password
         });
+        
     } catch (error) {
-        return reply.status(409).send();
+        if(error instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({message: error.message});
+        }
+
+        throw error;
     }
 
     return reply.status(201).send();
