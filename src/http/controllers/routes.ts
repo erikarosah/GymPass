e7da27ce73721738bd0fef20e8ad0a7bc4c908ca
erikarosah <erikarosamg@gmail.com>
2024-01-04 13:54:43 -0300

    Testes e2e

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -2,10 +2,11 @@ import { FastifyInstance } from 'fastify';
 import { profile } from './profile';
 import { register } from './register';
 import { authenticate } from './authenticate';
+import { verifyJWT } from '../middlewares/verify-jwt';
 
 export async function appRoutes (app: FastifyInstance) {
     app.post('/users', register);
     app.post('/sessions', authenticate);
     
-    app.get('/me', profile);
+    app.get('/me', {onRequest: [verifyJWT]}, profile);
 }
