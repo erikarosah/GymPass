ae61a47eef60787b22d9ae19fc1a5c7cfa2575dc
erikarosah <erikarosamg@gmail.com>
2024-01-05 13:10:52 -0300

    Autorização por cargos

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { history } from './history';
 import { validate } from './validate';
 import { FastifyInstance } from 'fastify';
 import { verifyJWT } from '../../middlewares/verify-jwt';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
 
 export async function checkInsRoutes (app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT);
@@ -11,5 +12,8 @@ export async function checkInsRoutes (app: FastifyInstance) {
     app.get('/check-ins/history', history);
     app.get('/check-ins/metrics', metrics);
     app.post('/gyms/:gymId/check-ins', create);
-    app.patch('/check-ins/:checkInId/validate', validate);
+    
+    app.patch('/check-ins/:checkInId/validate',
+        {onRequest: [verifyUserRole('ADMIN')]},
+        validate);
 }
